2bf2a79261cae544666f047d9ac7cbee05f27f13
ShivaKrishnaReddyBurra <reddyburrashivakrishna@gmail.com>
2024-05-09 13:30:54 +0530

    added few items

--- src/images/items2.jsx
+++ src/images/items2.jsx
@@ -5,6 +5,14 @@ const items2=[
         content: "Get Wave Astra BT Calling Smartwatch @ Rs 1499 Worth Rs 6999 only on boAt",
         code:"FLASH500 to get Extra Rs 500 Off",
         link:"https://bitli.in/3SJcaln"
+    },
+    {
+        id: 2,
+        img: require("./img13.jpg"),
+        content: "Get Airdopes Atom 83 @ Rs 999 Worth Rs 3490 only on boAt",
+        code:" FLASH500 to avail the offer",
+        link:"https://bitli.in/vcWQfta"
+
     }
 ]
 
