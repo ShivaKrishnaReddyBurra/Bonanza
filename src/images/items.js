103eb83ae0daff76284edaf4c70bb9959904cf8b
ShivaKrishnaReddyBurra <reddyburrashivakrishna@gmail.com>
2024-05-09 13:46:59 +0530

    added few items

--- src/images/items.js
+++ src/images/items.js
@@ -1,63 +1,57 @@
 const items=[
     {
         id:1,
-        img:require("./img1.jpg"),
-        content:"Get Max & Styli Starting @ Rs 199 only on Ajio",
-        link:"https://ajiio.in/cTpn3yZ "
-    },
-    {
-        id:2,
         img:require("./img2.jpg"),
         content:"Get Bajaj Mini Table Fan @ Rs 999 Worth Rs 1620 only on Croma!",
         link:"https://bitli.in/AsNM1dz"
     },
     {
-        id:3,
-        img:require("./img3.jpg"),
-        content:"Get Ayurvedic Onion Oil @ Rs 308 Worth Rs 499 only on Zandu",
-        link:"https://bitli.in/xUZqv1b "
-    },
-    {
-        id:4,
-        img:require("./img4.jpg"),
-        content:"Get BSC Charcoal Facewash and Peel Off Mask Combo @ Rs 179 worth Rs 608 only on BuyKaro!",
-        link:"https://bitli.in/YQ8Oyjh"
-    },
-    {
-        id:5,
+        id:2,
         img:require("./img5.jpg"),
         content:"FASHION CARNIVAL IS LIVE! Get Men's Branded Sportswear Under Rs 799 only on Myntra",
         link:"https://myntr.it/PhsNMqH "
     },
     {
-        id:6,
+        id:3,
         img:require("./img6.jpg"),
         content:"Get Upto 70% OFF On Mens Wallets only on Myntra ",
         link:"https://myntr.it/vcdvCDn"
     },
     {
-        id:7,
+        id:4,
         img:require("./img7.jpg"),
         content:"Get Flat 70% Off Across Categories only on Myntra",
         link:"https://myntr.it/ypaFlWT"
     },
     {
-        id:8,
+        id:5,
         img:require("./img8.jpg"),
         content:"Get Top Selling AC's Starting from Rs 25,990 only on Croma!",
         link:"https://bitli.in/pR1dVr0"
     },
     {
-        id:9,
+        id:6,
         img:require("./img11.jpg"),
         content:"Get Sun Care Products Under Rs 999 only on Nykaa",
         link:"https://bitli.in/NzZ8Ojf"
     },
     {
-        id:10,
+        id:7,
+        img:require("./img4.jpg"),
+        content:"Get Smartwatches Under Rs 999 only on Flipkart",
+        link:"https://fktr.in/lo8USwD"
+    },
+    {
+        id:8,
         img:require("./img12.jpg"),
-        content:"Get Tshirts from Roadster Under Rs 299 ",
-        link:"https://myntr.it/UnPb0dd"
+        content:"Get Bluetooth Speakers Under Rs 999 only on Flipkart",
+        link:"https://fktr.in/06XO50b"
+    },
+    {
+        id:9,
+        img:require("./img14.jpg"),
+        content:"Get Jeans Under Rs 999 only on Ajio",
+        link:"https://ajiio.in/lp620py"
     }
 ]
 
--- src/images/items3.js
+++ src/images/items3.js
@@ -4,6 +4,18 @@ const items3=[
         img: require("./img10.jpg"),
         content:"Get Chocolate Gift Combos Under Rs 499 only on Flipkart",
         link:"https://fktr.in/H9WiviC"
+    },
+    {
+        id:2,
+        img:require("./img1.jpg"),
+        content:"Get Double Door Refrigerators Starting from Rs 13,490 only on Flipkart",
+        link:"https://fktr.in/n8bg2Es"
+    },
+    {
+        id:3,
+        img:require("./img3.jpg"),
+        content:"Get Backpacks Under Rs 999 only on Flipkart",
+        link:"https://fktr.in/xFnyNmW"
     }
 ]
 export default items3;
