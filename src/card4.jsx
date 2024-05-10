c80f26b763478d08d655827c42f9ebdc76bd95a8
ShivaKrishnaReddyBurra <reddyburrashivakrishna@gmail.com>
2024-05-10 07:31:10 +0530

    added items

--- src/card4.jsx
+++ src/card4.jsx
@@ -8,7 +8,7 @@ function Card4(props) {
             <img src={props.img} alt="1" />
             <p>{props.content}</p>
             <p>{props.cost}</p>
-            <p>M.R.P:  &#8377;{props.orginalcost}</p>
+            <p>M.R.P:  &#8377;<span className="strike">{props.orginalcost}</span></p>
             <p>EMI starts at  &#8377;{props.emi}.No Cost EMI available EMI options</p>
             <a href={props.link}>{props.link}</a>
         </div>
